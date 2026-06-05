12a93cee30d60fdba184028aa30bdd4474fb7ef0
Ayush Ranjan Ojha <ayushojha.dev@gmail.com>
2026-06-05 09:31:29 +0530

    CORS updated for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Without this, browsers usually block the request with CORS and Axios shows "Network Error".
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://customer-churn-prediction-dusky.vercel.app/"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://customer-churn-prediction-dusky.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
